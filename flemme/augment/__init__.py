e72648775cf272f162d39568b0d6be4f076e6029
YaoLab666 <newcount66666@gmail.com>
2026-06-06 14:33:52 +0800

    Add files via upload

--- flemme/augment/__init__.py
+++ flemme/augment/__init__.py
@@ -1,1 +1,91 @@
-11
+from . import pcd_transforms
+from flemme.utils import DataForm, get_class, move_to_front, contains_one_of
+from flemme.logger import get_logger
+
+logger = get_logger('flemme.augment')
+
+
+class Compose:
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, data):
+        for transform in self.transforms:
+            data = transform(data)
+        return data
+
+seg_label_transform_table = {
+    'pcd': ['fixed', 'shuffle', 'totensor'],
+}
+
+random_transforms = ['FixedPoints', 'ShufflePoints']
+
+
+def get_transforms(trans_config_list, data_form=DataForm.PCD, img_dim=None):
+    transforms = []
+    contain_fixed_points = False
+    if len(trans_config_list) <= 0:
+        return Compose(transforms)
+    if data_form != DataForm.PCD:
+        logger.error(f'Unsupported data form for MedMamba transforms: {data_form}')
+        exit(1)
+
+    contain_fixed_points = move_to_front(
+        trans_config_list,
+        func=lambda a: a['name'] == 'FixedPoints',
+    )
+    for tc in trans_config_list:
+        trans_config = tc.copy()
+        trans_name = trans_config.pop('name')
+        trans_class = get_class(trans_name, pcd_transforms)
+        if trans_class is None:
+            logger.error(f"Unsupported point-cloud transform: {trans_name}")
+            exit(1)
+        transforms.append(trans_class(**trans_config))
+
+    transforms = Compose(transforms)
+    transforms.fixed_points = contain_fixed_points
+    return transforms
+
+
+def check_random_transforms(data_trans_config_list, label_trans_config_list):
+    data_random_transform_list = [
+        t['name'] for t in data_trans_config_list
+        if t['name'] in random_transforms or 'random' in t['name'].lower() or 'noise' in t['name'].lower()
+    ]
+    label_random_transform_list = [
+        t['name'] for t in label_trans_config_list
+        if t['name'] in random_transforms or 'random' in t['name'].lower() or 'noise' in t['name'].lower()
+    ]
+    if len(data_random_transform_list) < len(label_random_transform_list):
+        logger.warning('Label transforms introduce more random operations, are you sure this is what you want?')
+    for drt, lrt in zip(data_random_transform_list, label_random_transform_list):
+        if drt != lrt:
+            logger.error('Transforms that introduce random operations should have the same order for data and label.')
+            exit(1)
+
+
+def check_consistent_transforms(data_trans_config_list, label_trans_config_list, data_form):
+    if data_form != DataForm.PCD:
+        logger.error(f'Unsupported data form for MedMamba transforms: {data_form}')
+        exit(1)
+    selector = seg_label_transform_table['pcd']
+    data_consistent_transform_list = [
+        t['name'] for t in data_trans_config_list
+        if contains_one_of(t['name'].lower(), selector)
+    ]
+    label_consistent_transform_list = [
+        t['name'] for t in label_trans_config_list
+        if contains_one_of(t['name'].lower(), selector)
+    ]
+    if data_consistent_transform_list != label_consistent_transform_list:
+        logger.error('Transforms that change the shape or order of data should be consistent.')
+        exit(1)
+
+
+def select_label_transforms(trans_config_list, data_form):
+    if data_form != DataForm.PCD:
+        logger.error(f'Unsupported data form for MedMamba transforms: {data_form}')
+        exit(1)
+    selector = seg_label_transform_table['pcd']
+    return [t for t in trans_config_list if contains_one_of(t['name'].lower(), selector)]
---
+++ flemme/augment/pcd_transforms.py
@@ -0,0 +1,296 @@
+import torch
+import numpy as np
+import math
+import random
+import numbers
+from itertools import repeat
+from flemme.utils import label_to_onehot, normalize, to_int
+from functools import partial
+import fpsample
+#### transfoms for point cloud
+## modified from diffusion-point-cloud
+
+### can be used by label
+class ToInt:
+    def __call__(self, m):
+        return to_int(m)
+
+
+class Relabel:
+    def __init__(self, map=[], offset=None):
+        self.map = map
+        self.offset = offset
+
+    def __call__(self, m):
+        m = to_int(m)
+        if self.offset is not None:
+            m = m + self.offset
+        elif len(self.map) > 0:
+            if type(m) == int:
+                if m in self.map:
+                    m = self.map(m)
+            else:
+                for kv in self.map:
+                    k, v = kv
+                    m[m == k] = v
+        else:
+            if torch.is_tensor(m):
+                _, unique_labels = torch.unique(m, return_inverse=True)
+            else:
+                _, unique_labels = np.unique(m, return_inverse=True)
+            m = unique_labels.reshape(m.shape)
+        return m
+
+
+class ToTensor(object):
+    r"""Numpy to tensor"""
+
+    def __call__(self, data):
+        ## automatically transfer to torch.float or torch.int64
+        if type(data) == float or type(data) == int:
+            return torch.tensor(data)
+        elif data.dtype == int or data.dtype == np.int32:
+            return torch.tensor(data).long()
+        return torch.tensor(data).float()
+
+class ShufflePoints(object):
+    r"""Shuffle order of points in point cloud
+    """
+    def __init__(self):
+        ...
+    def __call__(self, data):
+        rand_idx = np.arange(len(data))
+        np.random.shuffle(rand_idx)
+        return data[rand_idx]
+
+class ToOneHot:
+    """
+    To one hot label
+    """
+    def __init__(self, num_classes):
+        self.to_onehot = partial(label_to_onehot, 
+            num_classes = num_classes, 
+            channel_dim = -1)
+    def __call__(self, m):
+        if not type(m) == int:
+            assert m.ndim == 1 or m.ndim == 2, "Not a per-point label or class label"
+            if m.ndim == 2:
+                assert m.shape[-1] == 1, \
+                    "Label is a multi channel point cloud. Check if it's already a one hot embedding."
+                m = m[..., 0]
+        return self.to_onehot(m)
+
+# class ToInt:
+#     def __call__(self, m):
+#         return to_int(m)
+
+# class Relabel(IMGRelabel):
+#     def __call__(self, m):
+#         return super().__call__(m)
+
+class FixedPoints(object):
+    r"""Samples a fixed number of :obj:`num` points and features from a point
+    cloud.
+    Args:
+        num (int): The number of points to sample.
+        replace (bool, optional): If set to :obj:`False`, samples fixed
+            points without replacement. In case :obj:`num` is greater than
+            the number of points, duplicated points are kept to a
+            minimum. (default: :obj:`True`)
+    """
+
+    def __init__(self, num, replace=True, method = 'random', kdtree_height = 5):
+        self.num = num
+        self.replace = replace
+        self.fpsampler = None
+        if method == 'fps':
+            self.fpsampler = partial(fpsample.fps_sampling, n_samples = self.num)
+        elif method == 'qfps':
+            self.fpsampler = partial(fpsample.bucket_fps_kdline_sampling, n_samples = self.num, h = kdtree_height)
+        else:
+            assert method == 'random', f'Unsupported sample strategy: {method}, should be one of ["random", "fps", "qfps"].'
+    def __call__(self, data):
+        key = None
+        if type(data) == tuple:
+            data, key = data
+        point_num = len(data)
+        if point_num < self.num or self.fpsampler is None:
+            if self.replace:
+                choice = np.random.choice(point_num, self.num, replace=True)
+            else:
+                choice = torch.cat([
+                    torch.randperm(point_num)
+                    for _ in range(math.ceil(self.num / point_num))
+                ], dim=0)[:self.num]
+        else:
+            assert (data.ndim == 2 and data.shape[-1] == 3)\
+                or (key is not None and key.ndim == 2 and key.shape[-1] == 3),\
+                "Cannot perform the furthest point sampling because the input is not a 3D point cloud."
+            if key is not None:
+                choice = self.fpsampler(key)
+            else:
+                choice = self.fpsampler(data)
+        return data[choice]
+
+class Normalize(object):
+    r"""Centers and normalizes node positions to the interval :math:`(-1, 1)`.
+    """
+
+    def __init__(self, method = 'minmax'):
+        self.method = method
+
+    def __call__(self, data):
+        return normalize(data, channel_dim = -1, method = self.method)
+
+class LinearTransformation(object):
+    r"""Transforms node positions with a square transformation matrix computed
+    offline.
+    Args:
+        matrix (Tensor): tensor with shape :math:`[D, D]` where :math:`D`
+            corresponds to the dimensionality of node positions.
+    """
+
+    def __init__(self, matrix):
+        assert matrix.dim() == 2, (
+            'Transformation matrix should be two-dimensional.')
+        assert matrix.size(0) == matrix.size(1), (
+            'Transformation matrix should be square. Got [{} x {}] rectangular'
+            'matrix.'.format(*matrix.size()))
+
+        self.matrix = matrix
+
+    def __call__(self, data):
+        data = torch.matmul(data, self.matrix.to(data.dtype).to(data.device))
+        return data
+
+class Rotate(object):
+    r"""Rotates node positions around a specific axis.
+    Args:
+        degrees (tuple or float): rotation degree
+        axis (int, optional): The rotation axis. (default: :obj:`0`)
+    """
+
+    def __init__(self, degree, axis=0):
+        self.degree = degree
+        self.axis = axis
+
+    def __call__(self, data):
+        degree = math.pi * self.degree / 180.0
+        sin, cos = math.sin(degree), math.cos(degree)
+
+        if self.axis == 0:
+            matrix = [[1, 0, 0], [0, cos, sin], [0, -sin, cos]]
+        elif self.axis == 1:
+            matrix = [[cos, 0, -sin], [0, 1, 0], [sin, 0, cos]]
+        else:
+            matrix = [[cos, sin, 0], [-sin, cos, 0], [0, 0, 1]]
+        return LinearTransformation(torch.tensor(matrix), )(data)
+
+class RandomRotate(object):
+    r"""Rotates node positions around a specific axis by a randomly sampled
+    factor within a given interval.
+    Args:
+        degrees (tuple or float): Rotation interval from which the rotation
+            angle is sampled. If :obj:`degrees` is a number instead of a
+            tuple, the interval is given by :math:`[-\mathrm{degrees},
+            \mathrm{degrees}]`.
+        axis (int, optional): The rotation axis. (default: :obj:`0`)
+    """
+
+    def __init__(self, degrees, axis=0):
+        if isinstance(degrees, numbers.Number):
+            degrees = (-abs(degrees), abs(degrees))
+        assert isinstance(degrees, (tuple, list)) and len(degrees) == 2
+        self.degrees = degrees
+        self.axis = axis
+
+    def __call__(self, data):
+        degree = math.pi * random.uniform(*self.degrees) / 180.0
+        sin, cos = math.sin(degree), math.cos(degree)
+        ## get transformation matrix
+        if self.axis == 0:
+            matrix = [[1, 0, 0], [0, cos, sin], [0, -sin, cos]]
+        elif self.axis == 1:
+            matrix = [[cos, 0, -sin], [0, 1, 0], [sin, 0, cos]]
+        else:
+            matrix = [[cos, sin, 0], [-sin, cos, 0], [0, 0, 1]]
+        return LinearTransformation(torch.tensor(matrix))(data)
+
+
+class AddNoise(object):
+
+    def __init__(self, std=0.01):
+        self.std = std
+
+    def __call__(self, data):
+        data = data + torch.normal(mean=0, std=self.std, size=data.size())
+        return data
+
+## std is random
+class AddRandomNoise(object):
+    def __init__(self, std_range=[0, 0.10]):
+        self.std_range = std_range
+
+    def __call__(self, data):
+        noise_std = random.uniform(*self.std_range)
+        data = data + torch.normal(mean=0, std=noise_std, size=data.size())
+        return data
+
+
+class RandomScale(object):
+    r"""Scales node positions by a randomly sampled factor :math:`s` within a
+    given interval, *e.g.*, resulting in the transformation matrix
+    .. math::
+        \begin{bmatrix}
+            s & 0 & 0 \\
+            0 & s & 0 \\
+            0 & 0 & s \\
+        \end{bmatrix}
+    for three-dimensional positions.
+    Args:
+        scales (tuple): scaling factor interval, e.g. :obj:`(a, b)`, then scale
+            is randomly sampled from the range
+            :math:`a \leq \mathrm{scale} \leq b`.
+    """
+
+    def __init__(self, scales):
+        assert isinstance(scales, (tuple, list)) and len(scales) == 2
+        self.scales = scales
+
+    def __call__(self, data):
+        scale = random.uniform(*self.scales)
+        data = data * scale
+        return data
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, self.scales)
+
+
+class RandomTranslate(object):
+    r"""Translates node positions by randomly sampled translation values
+    within a given interval. In contrast to other random transformations,
+    translation is applied separately at each position.
+    Args:
+        translate (sequence or float or int): Maximum translation in each
+            dimension, defining the range
+            :math:`(-\mathrm{translate}, +\mathrm{translate})` to sample from.
+            If :obj:`translate` is a number instead of a sequence, the same
+            range is used for each dimension.
+    """
+
+    def __init__(self, translate):
+        self.translate = translate
+
+    def __call__(self, data):
+        (n, dim), t = data.size(), self.translate
+        if isinstance(t, numbers.Number):
+            t = list(repeat(t, times=dim))
+        assert len(t) == dim
+
+        ts = []
+        for d in range(dim):
+            ts.append(data.new_empty(n).uniform_(-abs(t[d]), abs(t[d])))
+
+        data = data + torch.stack(ts, dim=-1)
+
+        return data
