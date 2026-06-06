2e107093c168d877d67a7063f50db49fc03ab720
YaoLab666 <newcount66666@gmail.com>
2026-06-06 14:40:17 +0800

    Add files via upload

--- flemme/cpp_extension/pcd_ops/include/ball_query.h
+++ flemme/cpp_extension/pcd_ops/include/ball_query.h
@@ -1,1 +1,8 @@
-11
+#pragma once
+#include <torch/extension.h>
+#include <vector>
+
+std::vector<at::Tensor> ball_query(at::Tensor new_xyz, at::Tensor xyz, const float radius,
+                      const int nsample);
+std::vector<at::Tensor> batch_ball_query(at::Tensor new_xyz, at::Tensor xyz, at::Tensor radius,
+                      const int nsample);
